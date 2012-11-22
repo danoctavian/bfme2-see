e15225fcbae0f3eaeb3af64aa94a852e31201704
bartdebree@hotmail.com <bartdebree@hotmail.com@b2d50534-d447-2895-1a53-1ec4ed9baf47>
2012-11-22 22:20:50 +0000

    Changed the .bat file...

--- trunk/AssetCacheBuilder.bat
+++ trunk/AssetCacheBuilder.bat
@@ -1,4 +1,6 @@
 @move art\compiledtextures art\textures
+@move art\textures art\ttextures
 @AssetCacheBuilderrc.exe
 @move art\textures art\compiledtextures
+@move art\ttextures art\textures
 @pause
