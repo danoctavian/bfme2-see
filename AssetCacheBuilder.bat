7a2575752cfa47c47ed228bfb04cd8b2cd87b894
bartdebree <bartdebree@hotmail.com>
2012-11-29 10:38:13 +0000

    Myyyy baaaad, didn't make the .bat correctly XD

--- AssetCacheBuilder.bat
+++ AssetCacheBuilder.bat
@@ -1,5 +1,5 @@
-@move art\compiledtextures art\textures
 @move art\textures art\ttextures
+@move art\compiledtextures art\textures
 @AssetCacheBuilderrc.exe
 @move art\textures art\compiledtextures
 @move art\ttextures art\textures
